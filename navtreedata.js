a529ab5cd24592a07626efd21a92d3fe65cb9b83
Antonio Augusto Alves Junior <aalvesju@gmail.com>
2016-10-10 16:47:13 +0200

    documentation

--- navtreedata.js
+++ navtreedata.js
@@ -1,11 +1,47 @@
 var NAVTREE =
 [
-  [ "Hydra", "index.html", ]
+  [ "Hydra", "index.html", [
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"index.html"
+"_add_pdf_8h.html",
+"_utility___tuple_8h.html#a85f5a7623a17a4f0c614e57cf4e2b2ae",
+"classhydra_1_1_vegas.html#a1429836e94a38d48b49fe5fc3016dbd7",
+"structhydra_1_1_base_functor.html#ab1401d548e112dbf4650f6edd24bd4ea",
+"structhydra_1_1_parameter.html",
+"structhydra_1_1detail_1_1make__index__sequence_3_010_01_4.html#a6f2ab185f1f88a0d0ff5407c12710cfc"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
