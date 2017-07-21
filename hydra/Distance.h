7aeef692daf08a754cc095a01730215a6ca9a9d9
Augusto <augalves@LabHome>
2017-07-21 10:24:13 +0200

    documentation, examples, random...

---
+++ hydra/Distance.h
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------
+ *
+ *   Copyright (C) 2016 Antonio Augusto Alves Junior
+ *
+ *   This file is part of Hydra Data Analysis Framework.
+ *
+ *   Hydra is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Hydra is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Hydra.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *---------------------------------------------------------------------------*/
+
+/*
+ * Distance.h
+ *
+ *  Created on: 21/07/2017
+ *      Author: Antonio Augusto Alves Junior
+ */
+
+#ifndef DISTANCE_H_
+#define DISTANCE_H_
+
+#include <hydra/detail/Config.h>
+#include <hydra/detail/BackendPolicy.h>
+#include <hydra/Types.h>
+#include <thrust/distance.h>
+
+namespace hydra {
+
+template<typename Iterator>
+inline __host__ __device__
+auto distance(Iterator first, Iterator last)
+->decltype( thrust::distance<Iterator>(first,last))
+{
+	return thrust::distance(first, last);
+}
+
+}  // namespace hydra
+
+#endif /* DISTANCE_H_ */
