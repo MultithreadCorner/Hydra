3d7f0f679c62f2a0937622d44b956a19b2db72f9
Augusto <augalves@LabHome>
2017-10-18 12:00:10 +0200

    complex numbers, placeholders and advanced casting...

--- hydra/Complex.h
+++ hydra/Complex.h
@@ -43,3 +43,169 @@ using Complex =  HYDRA_EXTERNAL_NS::thrust::complex<T>;
 }  // namespace hydra
 
 #endif /* COMPLEX_H_ */
+
+/*
+
+ template<typename T >
+__host__ __device__ T 	thrust::abs (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ T 	thrust::arg (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ T 	thrust::norm (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::conj (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::polar (const T &m, const T &theta=0)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::proj (const T &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator* (const complex< T > &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator* (const complex< T > &lhs, const T &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator* (const T &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator/ (const complex< T > &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator/ (const complex< T > &lhs, const T &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator/ (const T &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator+ (const complex< T > &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator+ (const complex< T > &lhs, const T &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator+ (const T &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator- (const complex< T > &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator- (const complex< T > &lhs, const T &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator- (const T &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator+ (const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::operator- (const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::exp (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::log (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::log10 (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::pow (const complex< T > &x, const complex< T > &y)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::pow (const complex< T > &x, const T &y)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::pow (const T &x, const complex< T > &y)
+
+template<typename T , typename U >
+__host__ __device__ complex
+< typename
+detail::promoted_numerical_type
+< T, U >::type > 	thrust::pow (const complex< T > &x, const complex< U > &y)
+
+template<typename T , typename U >
+__host__ __device__ complex
+< typename
+detail::promoted_numerical_type
+< T, U >::type > 	thrust::pow (const complex< T > &x, const U &y)
+
+template<typename T , typename U >
+__host__ __device__ complex
+< typename
+detail::promoted_numerical_type
+< T, U >::type > 	thrust::pow (const T &x, const complex< U > &y)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::sqrt (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::cos (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::sin (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::tan (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::cosh (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::sinh (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::tanh (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::acos (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::asin (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::atan (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::acosh (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::asinh (const complex< T > &z)
+
+template<typename T >
+__host__ __device__ complex< T > 	thrust::atanh (const complex< T > &z)
+
+template<typename ValueType , class charT , class traits >
+std::basic_ostream< charT,traits > &
+thrust::operator<< (std::basic_ostream< charT, traits > &os, const complex< ValueType > &z)
+
+template<typename ValueType , typename charT , class traits >
+std::basic_istream< charT,traits > &
+thrust::operator>> (std::basic_istream< charT, traits > &is, complex< ValueType > &z)
+
+template<typename T >
+__host__ __device__ bool 	thrust::operator== (const complex< T > &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ bool 	thrust::operator== (const T &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ bool 	thrust::operator== (const complex< T > &lhs, const T &rhs)
+
+template<typename T >
+__host__ __device__ bool 	thrust::operator!= (const complex< T > &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ bool 	thrust::operator!= (const T &lhs, const complex< T > &rhs)
+
+template<typename T >
+__host__ __device__ bool 	thrust::operator!= (const complex< T > &lhs, const T &rhs)
+
+
+ */
