5a04ec9a26f62236571094b26a610c4d5390ed92
AAAlvesJr <aalvesju@gmail.com>
2018-02-18 00:46:23 +0100

    ...

--- hydra/GaussKronrodQuadrature.h
+++ hydra/GaussKronrodQuadrature.h
@@ -102,13 +102,9 @@ public:
 
 	typedef HYDRA_EXTERNAL_NS::thrust::tuple<double, double, double, double, double> row_t;
 
-	//typedef HYDRA_EXTERNAL_NS::thrust::host_vector<row_t>   row_list_h;
-	//typedef HYDRA_EXTERNAL_NS::thrust::device_vector<row_t> row_list_d;
 
-	typedef multivector<row_t, hydra::cpp::sys_t> table_h;
+	typedef multivector<row_t, hydra::host::sys_t> table_h;
 	typedef multivector<row_t, hydra::detail::BackendPolicy<BACKEND>> table_d;
-	//typedef multivector<row_list_h> table_h;
-	//typedef multivector<row_list_d> table_d;
 
 
 	GaussKronrodQuadrature(GReal_t xlower, GReal_t xupper):
